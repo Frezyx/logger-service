7035f58a1c4d10e527ed7e7b05d9594376c72964
Frezyx <youngfrezyx@gmail.com>
2020-11-18 05:03:03 +0300

    Update telegram_routes.js

--- routes/telegram_routes.js
+++ routes/telegram_routes.js
@@ -4,11 +4,18 @@ const router = Router()
 
 router.post('/auth', async (req, res) => {
     try {
-        const chat = new TelegramChat({
+        const prevChat = await TelegramChat.findOne({
             id: req.body.chatId,
         })
-        await chat.save()
-        res.status(201).send('Create new chat')
+        if (prevChat == null){
+            const chat = new TelegramChat({
+                id: req.body.chatId,
+            })
+            await chat.save()
+            res.status(201).send('Create new chat')
+        } else {
+            res.status(200).send('Chat already created')
+        }
     } catch (error) {
         res.status(500).send('Chat is not created')
     }
@@ -17,6 +24,15 @@ router.post('/auth', async (req, res) => {
 router.get('/get', async (_, res) => {
     const chats = await TelegramChat.find({})
     res.send(chats)
+})
+
+router.delete('/delete-all', async (_, res) => {
+    try {
+        await TelegramChat.deleteMany({})
+        res.status(200).send('Deleted all Chats')
+    } catch (error) {
+        res.status(500).send('Chats is not deleted')
+    }
 })
 
 
